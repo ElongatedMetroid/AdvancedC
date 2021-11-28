75c4bef7b8d7f65ad3e1415054a5b0ea2b679339
Nate <nategamedevelopmentacc@gmail.com>
2021-11-27 19:23:31 -0800

    complex num challange and ex1

---
+++ AdvancedDataTypes/complexNumType/challange.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+
+int main()
+{
+    double complex cx = (I*I);
+    printf("cx: Real, %.2f Imaginary, %.2fi\n", creal(cx), cimag(cx));
+
+    double complex cy = pow(I, 2);
+    printf("cy: Real, %.2f Imaginary, %2fi\n", creal(cy), cimag(cy));
+
+    double complex cz = I * M_PI;
+    printf("cz: Real, %.2f Imaginary, %2fi\n", creal(cz), cimag(cz));
+
+    double complex ca = 1+2*I;
+    double complex cb = 1-2*I;
+    printf("ca: Real, %.2f Imaginary, %2fi\n", creal(ca), cimag(ca));
+    printf("cb: Real, %.2f Imaginary, %2fi\n", creal(cb), cimag(cb));
+    
+    return 0;
+}
---
+++ AdvancedDataTypes/complexNumType/ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <complex.h>
+
+#define __STDC_WANT_LIB_EXT1__
+
+int main()
+{
+    #ifdef __STDC_NO_COMPLEX__
+        printf("Complex numbers are not supported.\n");
+        exit(1);
+    #else
+        printf("Complex numbers are supported.\n\n");
+
+        double complex cx = 1.0 + 3.0*I;
+        double complex cy = 1.0 - 4.0*I;
+
+        printf("Working with complex numbers:\n");
+        printf("Starting values: cx = %.2f%+.2fi cy = %.2f%+.2fi\n", creal(cx), cimag(cx), creal(cy), cimag(cy));
+
+        double complex sum = cx + cy;
+        printf("\nThe sum cx + cy = %.2f%+.2fi\n", creal(sum), cimag(sum));
+    #endif
+
+    return 0;
+}
